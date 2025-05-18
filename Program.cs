474eb00c521ac6d50ef1253c56d5e41a9f96f1a4
rohithv1997 <rohithv97@outlook.com>
2025-05-18 22:21:31 +0530

    chore: wasm migration

--- Program.cs
+++ Program.cs
@@ -1,32 +1,16 @@
+using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using PortfolioV2;
 using MudBlazor.Services;
-using PortfolioV2.Components;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add MudBlazor services
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddMudServices();
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-
-app.UseAntiforgery();
-
-app.MapStaticAssets();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
+await builder.Build().RunAsync();
